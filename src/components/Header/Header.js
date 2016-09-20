4150ae3f56130eb1a174e880282610b77dff2fbd
ShanzayA <shanzayamjad@hotmail.com>
2016-09-20 11:33:36 +0200

    Initial Commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+/**
+ * React Starter Kit (https://www.reactstarterkit.com/)
+ *
+ * Copyright © 2014-2016 Kriasoft, LLC. All rights reserved.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt file in the root directory of this source tree.
+ */
+
+import React, { Component } from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './Header.scss';
+import Link from '../Link';
+import Navigation from '../Navigation';
+
+class Header extends Component {
+
+  render() {
+    return (
+      <div className={s.root}>
+        <div className={s.container}>
+          <Navigation className={s.nav} />
+          <Link className={s.brand} to="/">
+            <img src={require('./WDAquaLogo2.png')} height="38" alt="WDAqua" />
+          </Link>
+          <div className={s.banner}>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default withStyles(Header, s);
