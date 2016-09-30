2512bff43f409c1055ab442bb4d7ef575e13d7a6
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2016-09-30 14:46:10 +0200

    Literal and ask cases handeled

--- src/components/AnswerPage/AnswerPage.js
+++ src/components/AnswerPage/AnswerPage.js
@@ -39,75 +39,74 @@ class AnswerPage extends Component {
       //would like to refactor the following to separate functions. this.setState() is not recognized in
       //nested functions
 
-
-      var gerbilResult={"questions":[{"question":{"answers":"{   \"head\": {     \"vars\": [ \"x\" ]   } ,   \"results\": {     \"bindings\": [       {         \"x\": { \"type\": \"uri\" , \"value\": \"http://dbpedia.org/resource/Michelle_Obama\" }       }     ]   } } ","language":[{"SPARQL":"SELECT DISTINCT ?x WHERE {    <http://dbpedia.org/resource/Barack_Obama> <http://dbpedia.org/property/spouse> ?x .    ?x <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://dbpedia.org/ontology/Agent> .  }  limit 1000"}]}}]};
-
       var jresult = JSON.parse(data.questions[0].question.answers);
-
-      // console.log("json parse printtttttt");
-      // console.log("result count: " + jresult.results.bindings.length);
-      // console.log("results: ");
-      // console.log(jresult.results.bindings[0].x);
 
       console.log("json");
       console.log(jresult);
       console.log("result count: " + jresult.results.bindings.length);
       console.log("results: ");
 
+      //check if it is an ask query
+      if (jresult.hasOwnProperty("boolean")){
+        this.setState({
+          label: jresult.boolean,
+        })
+      } else {
+        //depending on the number of results, handle accordingly:
+        if (jresult.results.bindings.length == 1) {
 
-      //depending on the number of results, handle accordingly:
-      if (jresult.results.bindings.length == 1){
+          console.log("Variable" + jresult.head.vars[0]);
+          var variable = jresult.head.vars[0];
 
-        console.log("Variable"+jresult.head.vars[0]);
-        var variable=jresult.head.vars[0];
+          var type = jresult.results.bindings[0][variable].type;
+          var value = jresult.results.bindings[0][variable].value;
+          console.log("Result " + type + " " + value);
 
-        var type=jresult.results.bindings[0][variable].type;
-        var value=jresult.results.bindings[0][variable].value;
-        console.log("Result "+type+" "+value);
+          if (type == "uri") {
+            //There is only one uri
+            var sparqlQuery = "select ?label ?abstract ?image where { "
+              + "<" + value + "> rdfs:label ?label . "
+              + " OPTIONAL{ "
+              + "<" + value + "> dbo:thumbnail ?image . "
+              + "} "
+              + " OPTIONAL{ "
+              + "<" + value + "> dbo:abstract ?abstract . "
+              + "} "
+              + " FILTER (lang(?label)=\"en\" && lang(?abstract)=\"en\") "
+              + " } ";
 
-        if (type=="uri"){
-          //There is only one uri
-          var sparqlQuery="select ?label ?abstract ?image where { "
-            + "<"+value+"> rdfs:label ?label . "
-            + " OPTIONAL{ "
-            + "<"+value+"> dbo:thumbnail ?image . "
-            + "} "
-            + " OPTIONAL{ "
-            + "<"+value+"> dbo:abstract ?abstract . "
-            + "} "
-            + " FILTER (lang(?label)=\"en\" && lang(?abstract)=\"en\") "
-            + " } ";
-
-          this.serverRequest = $.get(
-            "http://dbpedia.org/sparql?query="+encodeURIComponent(sparqlQuery)+"&format=application%2Fsparql-results%2Bjson&CXML_redir_for_hrefs=&timeout=30000&debug=on",
-            function (result) {
-              console.log(result);
-              console.log(result.results.bindings[0].label.value);
-              console.log(result.results.bindings[0].image.value);
-              console.log(result.results.bindings[0].abstract.value);
-              this.setState({ label : result.results.bindings[0].label.value,
-                abstract : result.results.bindings[0].abstract.value,
-                image : result.results.bindings[0].image.value});
-            }.bind(this), "json")
+            this.serverRequest = $.get(
+              "http://dbpedia.org/sparql?query=" + encodeURIComponent(sparqlQuery) + "&format=application%2Fsparql-results%2Bjson&CXML_redir_for_hrefs=&timeout=30000&debug=on",
+              function (result) {
+                console.log(result);
+                console.log(result.results.bindings[0].label.value);
+                console.log(result.results.bindings[0].image.value);
+                console.log(result.results.bindings[0].abstract.value);
+                this.setState({
+                  label: result.results.bindings[0].label.value,
+                  abstract: result.results.bindings[0].abstract.value,
+                  image: result.results.bindings[0].image.value
+                });
+              }.bind(this), "json")
 
 
+          }
+          else if (type == "typed-literal" || type =="literal") {
+            console.log("Here" + jresult.results.bindings[0][variable].value);
+            this.setState({
+              label: jresult.results.bindings[0][variable].value,
+            });
+          }
         }
-        else if (type=="typed-literal") {
-          console.log("Here"+jresult.results.bindings[0][variable].value);
+        else if (jresult.results.bindings.length > 1) {
+          //do something
+        }
+        else {
           this.setState({
-            label : jresult.results.bindings[0][variable].value,
+            label: "No results",
           });
         }
       }
-
-      else if (jresult.results.bindings.length > 1) {
-        //do something
-      }
-
-      else {
-        //if there are no results...
-      }
-
     }.bind(this), "json");
 
     // qresult.done(function (data){
@@ -124,10 +123,9 @@ class AnswerPage extends Component {
     console.log(this.state.label);
     return (
       <div className={s.container}>
-        <h1>Answer</h1>
-        < ImageComponent image={this.state.image}></ImageComponent>
         <Label>{this.state.label}</Label>
-        <Label>{this.state.abstract}</Label>
+        {(this.state.image == "") ? null : < ImageComponent image={this.state.image}></ImageComponent>}
+        {(this.state.abstract == "") ? null : <Label>{this.state.abstract}</Label>}
       </div>
     );
   }
