d15610a83905bfb7366a757c1e8b0b8c01c0021d
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2016-10-01 20:17:40 +0200

    Support for multiple answers added but restricted only to the first 10

--- src/components/AnswerPage/AnswerPage.js
+++ src/components/AnswerPage/AnswerPage.js
@@ -24,11 +24,8 @@ class AnswerPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      information: [],
       loaded: false,
-      label : "",
-      image : "",
-      abstract : "",
-      answertype : "simple"
     };
   }
 
@@ -48,63 +45,80 @@ class AnswerPage extends Component {
 
       //check if it is an ask query
       if (jresult.hasOwnProperty("boolean")){
-        this.setState({
+        var information = this.state.information;
+        information.push({
           label: (jresult.boolean==true) ? "True" : "False",
+          answerType: "simple",
+        })
+        this.setState({
+          information: information,
           loaded: true,
-          answertype: "simple"
         })
       } else {
+        var variable=jresult.head.vars[0];
         //depending on the number of results, handle accordingly:
-        if (jresult.results.bindings.length == 1) {
+        if(jresult.results.bindings.length < 200) {
+          jresult.results.bindings.map(function(binding,k) {
+            if (k<10) {
+              console.log("k:" + k);
+              console.log(variable);
+              //console.log("Variable" + jresult.head.vars[0]);
+              //var variable = jresult.head.vars[0];
 
-          console.log("Variable" + jresult.head.vars[0]);
-          var variable = jresult.head.vars[0];
+              var type = binding[variable].type;
+              var value = binding[variable].value;
+              console.log("Result " + type + " " + value);
 
-          var type = jresult.results.bindings[0][variable].type;
-          var value = jresult.results.bindings[0][variable].value;
-          console.log("Result " + type + " " + value);
+              if (type == "uri") {
+                //There is only one uri
+                var sparqlQuery = "select ?label ?abstract ?image where { "
+                  + "<" + value + "> rdfs:label ?label . "
+                  + " OPTIONAL{ "
+                  + "<" + value + "> dbo:thumbnail ?image . "
+                  + "} "
+                  + " OPTIONAL{ "
+                  + "<" + value + "> dbo:abstract ?abstract . "
+                  + "} "
+                  + " FILTER (lang(?label)=\"en\" && lang(?abstract)=\"en\") "
+                  + " } ";
 
-          if (type == "uri") {
-            //There is only one uri
-            var sparqlQuery = "select ?label ?abstract ?image where { "
-              + "<" + value + "> rdfs:label ?label . "
-              + " OPTIONAL{ "
-              + "<" + value + "> dbo:thumbnail ?image . "
-              + "} "
-              + " OPTIONAL{ "
-              + "<" + value + "> dbo:abstract ?abstract . "
-              + "} "
-              + " FILTER (lang(?label)=\"en\" && lang(?abstract)=\"en\") "
-              + " } ";
-
-            this.serverRequest = $.get(
-              "http://dbpedia.org/sparql?query=" + encodeURIComponent(sparqlQuery) + "&format=application%2Fsparql-results%2Bjson&CXML_redir_for_hrefs=&timeout=30000&debug=on",
-              function (result) {
-                console.log(result);
-                console.log(result.results.bindings[0].label.value);
-                console.log(result.results.bindings[0].image.value);
-                console.log(result.results.bindings[0].abstract.value);
-                this.setState({
-                  label: result.results.bindings[0].label.value,
-                  abstract: result.results.bindings[0].abstract.value,
-                  image: result.results.bindings[0].image.value,
+                this.serverRequest = $.get(
+                  "http://dbpedia.org/sparql?query=" + encodeURIComponent(sparqlQuery) + "&format=application%2Fsparql-results%2Bjson&CXML_redir_for_hrefs=&timeout=30000&debug=on",
+                  function (result) {
+                    console.log(result);
+                    console.log("Label" + result.results.bindings[0].label.value);
+                    console.log("Abstract" + result.results.bindings[0].abstract.value);
+                    var image = (result.results.bindings[0].image != undefined) ? result.results.bindings[0].image.value : "";
+                    var information = this.state.information;
+                    information.push({
+                      label: result.results.bindings[0].label.value,
+                      abstract: result.results.bindings[0].abstract.value,
+                      image: image,
+                      loaded: true,
+                      answertype: "detail"
+                    })
+                    this.setState({
+                      information: information,
+                      loaded: true,
+                    })
+                  }.bind(this), "json")
+              }
+              else if (type == "typed-literal" || type == "literal") {
+                var information = this.state.information;
+                information.push({
+                  label: jresult.results.bindings[0][variable].value,
                   loaded: true,
-                  answertype: "detail"
+                  answertype: "simple"
+                })
+                this.setState({
+                  information: information,
+                  loaded: true,
                 });
-              }.bind(this), "json")
-
-
-          }
-          else if (type == "typed-literal" || type =="literal") {
-            console.log("Here" + jresult.results.bindings[0][variable].value);
-            this.setState({
-              label: jresult.results.bindings[0][variable].value,
-              loaded: true,
-              answertype: "simple"
-            });
-          }
+              }
+            }
+          }.bind(this), "json")
         }
-        else if (jresult.results.bindings.length > 1) {
+        else if (jresult.results.bindings.length > 5) {
           //do something
         }
         else {
@@ -138,16 +152,30 @@ class AnswerPage extends Component {
     // }
     // else {}
 
+
+
+
+    //
+
 //to refactor so don't have to check the same answer type multiple times
+    console.log("Loaded "+this.state.loaded);
+    console.log("Information "+this.state.information.length);
     return (
       <div className={s.container}>
         <Loader loaded={this.state.loaded}>
-        {(this.state.answertype == "simple") ? <Label css={s.answer}>{this.state.label}</Label> : null}
-        {(this.state.answertype == "detail") ? <ImageComponent image={this.state.image}></ImageComponent> : null}
-        {(this.state.answertype == "detail") ? <div className={s.textboxes}>
-          <Label css={s.answer}>{this.state.label}</Label>
-          <Label>{this.state.abstract}</Label>
-        </div> : null}
+          {this.state.information.map(function(info,index) {
+            console.log("k"+index);
+            console.log(info);
+            return (
+              <div key={index} >
+                 {(info.answertype == "simple") ? <Label css={s.answer}>{info.label}</Label> : null}
+                 {(info.answertype == "detail") ? <ImageComponent image={info.image}></ImageComponent> : null}
+                 {(info.answertype == "detail") ? <div className={s.textboxes}>
+                 <Label css={s.answer}>{info.label}</Label>
+                 <Label>{info.abstract}</Label>
+                 </div> : null}
+              </div>)
+          }.bind(this), "json")}
         </Loader>
       </div>
     );
